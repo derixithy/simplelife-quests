2002e8b6e7251e8c74591c70163ed397f2380407
Derixithy <derixithy@usershare.nl>
2018-11-24 18:24:27 +0100

    Compatibility with notify

--- central_message.lua
+++ central_message.lua
@@ -1,8 +1,8 @@
-if (cmsg) then
+if (notify) then
 	function quests.show_message(t, playername, text)
 		if (quests.hud[playername].central_message_enabled) then
 			local player = minetest.get_player_by_name(playername)
-			cmsg.push_message_player(player, text, quests.colors[t])
+			notify.push_message_player(player, text, quests.colors[t])
 			minetest.sound_play("quests_" .. t, {to_player = playername})
 		end
 	end
